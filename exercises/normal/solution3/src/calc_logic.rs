4f816a4cc9e21c7e2e8020a457e6013864a3d5a0
jyy <1305081045@qq.com>
2025-02-08 23:42:14 +0000

    feature: implement normal solution 3 & 5

--- exercises/normal/solution3/src/calc_logic.rs
+++ exercises/normal/solution3/src/calc_logic.rs
@@ -1,4 +1,15 @@
+fn round_to_four_decimal_places(x: f64) -> f64 {
+    (x * 10000.0).round() / 10000.0
+}
+
 pub fn new_birthday_probability(n: u32) -> f64 {
-    // TODO: 这里写逻辑
-    todo!()
+    if n > 365 {
+        round_to_four_decimal_places(1.0)
+    } else {
+        let mut diff = 1.0;
+        for i in (365 - n + 1)..=365 {
+            diff *= i as  f64 / 365.0;
+        }
+        round_to_four_decimal_places(1.0 - diff)    
+    }
 }
--- exercises/normal/solution5/src/fibnacci.rs
+++ exercises/normal/solution5/src/fibnacci.rs
@@ -1,4 +1,36 @@
 pub fn odd_fibnacci_sum(threshold: u32) -> u32 {
-    // TODO: 这里写逻辑
-    todo!()
+    let mut v = vec![0;10];
+    let mut r = 0;
+    
+    let mut index  = 0;
+    
+    loop {
+        if r >= threshold {
+            break;
+        }
+        let mut current_fib = 0;
+        index = index + 1;
+        if index == 1 {
+            *v.get_mut(index).unwrap() = 1;
+            r = 1;
+            current_fib = 1;
+        } else {
+            current_fib = *v.get(index - 1).unwrap()
+                          +
+                          *v.get(index - 2).unwrap();
+            if current_fib % 2 == 1 {
+                r += current_fib;
+            }
+            match v.get_mut(index) {
+                Some(index_ref) => {
+                    *index_ref = current_fib;
+                },
+                None => {
+                    v.push(current_fib);
+                }
+            }
+        } 
+        
+    }
+    r
 }

8a98fead0b9917f0f513a73ed41432a03239899b
jyy <1305081045@qq.com>
2025-02-08 12:23:20 +0000

    feature: implement normal solution1

--- exercises/normal/solution1/src/count_distinct.rs
+++ exercises/normal/solution1/src/count_distinct.rs
@@ -1,3 +1,10 @@
 pub fn new_count_distinct(input_str: &str) -> usize {
-    todo!()
+    let v: Vec<&str> = input_str.split(',').collect();
+    let mut hash_set = std::collections::HashSet::new();
+
+    for i in v.iter() {
+        let s = String::from(*i);
+        hash_set.insert(s);
+    }
+    hash_set.len()
 }

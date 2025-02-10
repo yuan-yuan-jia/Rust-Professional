573332d7966a98ce261610354873b288d553a1f5
jyy <1305081045@qq.com>
2025-02-10 09:55:56 +0000

    feature: implement easy algorithm13

--- exercises/easy/algorithm13.rs
+++ exercises/easy/algorithm13.rs
@@ -13,9 +13,52 @@
 
 use std::fmt::{self, Display, Formatter};
 
+fn keep_alphabetical(s: String) -> String {
+    let mut r = String::new();
+    let temp_s = s.to_lowercase();
+    for c in temp_s.chars() {
+        if c >= 'a' && c <= 'z' {
+            r.push(c);
+        }
+    }
+    r
+}
+
 pub fn are_anagrams(s1: String, s2: String) -> bool {
     // TODO: Implement the logic to check if two strings are anagrams
-    false // Placeholder return value
+    let mut s1 = keep_alphabetical(s1);
+    let mut s2 = keep_alphabetical(s2);
+
+    if s1.len() != s2.len() {
+        return false;
+    }
+    let mut container = [0;26];
+    for i in s1.chars() {
+        let rd = container.get_mut(i as usize - 'a' as usize);
+        match rd {
+            Some(r) => {
+                *r = *r + 1;
+            },
+            None => panic!("should be unreacheable"),
+        }
+    }
+    
+    for i in s2.chars() {
+        let rd = container.get_mut(i as usize - 'a' as usize);
+        match rd {
+            Some(r) => {
+                *r = *r - 1;
+            },
+            None => panic!("should be unreacheable"),
+        }
+    }
+
+    for i in &container {
+        if *i != 0 {
+            return false;
+        }
+    }
+    true// Placeholder return value
 }
 
 #[cfg(test)]
